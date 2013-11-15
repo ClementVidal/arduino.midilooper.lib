fefff751a3afd01baf7e4bb9b23fe428bd9d6ef2
Clement Vidal <clementvidalperso@gmail.com>
2013-11-15 14:14:23 +0100

    Update build.sh and .gitignore

--- build.sh
+++ build.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-BUILD_DIR="./build"
+BUILD_DIR="$(pwd)/build"
 
 if [ -d "$BUILD_DIR" ]; then
     rm -rf "$BUILD_DIR"
 fi
 
 mkdir "$BUILD_DIR"
+mkdir "$BUILD_DIR/ArduinoCoreLib"
+mkdir "$BUILD_DIR/ArduinoMIDILib"
+mkdir "$BUILD_DIR/ArduinoSPILib"
+mkdir "$BUILD_DIR/ArduinoSPIRAMLib"
 
-cd "$BUILD_DIR"
-
-cmake -DCMAKE_INSTALL_PREFIX=~/Dev/ArduinoLib/Install/ ../ArduinoCoreLib && make all install
-cmake -DCMAKE_INSTALL_PREFIX=~/Dev/ArduinoLib/Install/ ../ArduinoMIDILib && make all install
-cmake -DCMAKE_INSTALL_PREFIX=~/Dev/ArduinoLib/Install/ ../ArduinoSPILib && make all install
-cmake -DCMAKE_INSTALL_PREFIX=~/Dev/ArduinoLib/Install/ ../ArduinoSPIRAMLib && make all install
+cd "$BUILD_DIR/ArduinoCoreLib"
+cmake -DCMAKE_INSTALL_PREFIX=~/Dev/ArduinoLib/install/ ../../ArduinoCoreLib && make all install
+cd "$BUILD_DIR/ArduinoMIDILib"
+cmake -DCMAKE_INSTALL_PREFIX=~/Dev/ArduinoLib/install/ ../../ArduinoMIDILib && make all install
+cd "$BUILD_DIR/ArduinoSPILib"
+cmake -DCMAKE_INSTALL_PREFIX=~/Dev/ArduinoLib/install/ ../../ArduinoSPILib && make all install
+cd "$BUILD_DIR/ArduinoSPIRAMLib"
+cmake -DCMAKE_INSTALL_PREFIX=~/Dev/ArduinoLib/install/ ../../ArduinoSPIRAMLib && make all install

469f05dfacfae9cfd4027fe44eef7e93f9a5c858
Clement Vidal <clementvidalperso@gmail.com>
2013-11-26 11:56:01 +0100

    Update Build.sh

--- build.sh
+++ build.sh
@@ -12,6 +12,7 @@ mkdir "$BUILD_DIR/ArduinoMIDILib"
 mkdir "$BUILD_DIR/ArduinoSPILib"
 mkdir "$BUILD_DIR/ArduinoSPIRAMLib"
 mkdir "$BUILD_DIR/ArduinoLCDLib"
+mkdir "$BUILD_DIR/ArduinoEEPROMLib"
 
 cd "$BUILD_DIR/ArduinoCoreLib"
 cmake -DCMAKE_INSTALL_PREFIX=${ARDUINO_INSTALL} ../../ArduinoCoreLib && make all install
@@ -23,3 +24,5 @@ cd "$BUILD_DIR/ArduinoSPIRAMLib"
 cmake -DCMAKE_INSTALL_PREFIX=${ARDUINO_INSTALL} ../../ArduinoSPIRAMLib && make all install
 cd "$BUILD_DIR/ArduinoLCDLib"
 cmake -DCMAKE_INSTALL_PREFIX=${ARDUINO_INSTALL} ../../ArduinoLCDLib && make all install
+cd "$BUILD_DIR/ArduinoEEPROMLib"
+cmake -DCMAKE_INSTALL_PREFIX=${ARDUINO_INSTALL} ../../ArduinoEEPROMLib && make all install
